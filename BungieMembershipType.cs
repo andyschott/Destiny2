8ef687d66fd4ee4f7cf34d305230d4f3e02a866d
Andy Schott <andy@andyschott.com>
2019-06-01 15:21:16 -0400

    add a description to Bungie account types

--- BungieMembershipType.cs
+++ BungieMembershipType.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel;
+
 namespace Destiny2
 {
     public enum BungieMembershipType
     {
+        [Description("None")]
         None = 0,
+        [Description("Xbox")]
         TigerXbox = 1,
+        [Description("PSN")]
         TigerPsn = 2,
+        [Description("Battle.NET")]
         TigerBlizzard = 4,
+        [Description("Demon")]
         TigerDemon = 10,
+        [Description("Bungie")]
         BungieNext = 254,
     }
 }
