6601fe8e33656989369fa126b0545b1fa2fd3c0a
Andy Schott <andy@andyschott.com>
2019-06-02 14:49:58 -0400

    add enum for all item slots

---
+++ ItemSlot.cs
@@ -0,0 +1,21 @@
+namespace Destiny2
+{
+    public enum ItemSlot : long
+    {
+        KineticWeapon = 1498876634,
+        EnergyWeapon = 2465295065,
+        PowerWeapon = 953998645,
+        Helmet = 3448274439,
+        Gauntlet = 3551918588,
+        ChestArmor = 14239492,
+        LegArmor = 20886954,
+        ClassArmor = 1585787867,
+        Ghost = 4023194814,
+        Vehicle = 2025709351,
+        Ship = 284967655,
+        Subclass = 3284755031,
+        ClanBanner = 4292445962,
+        Emblem = 4274335291,
+        Emotes = 1107761855,
+    }
+}
