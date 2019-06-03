cf7fbd830240fc850da8040fe388f36bb9218f59
Andy Schott <andy@andyschott.com>
2019-06-02 21:29:33 -0400

    expose more data when retrieving profile information

--- Responses/DestinyProfileResponse.cs
+++ Responses/DestinyProfileResponse.cs
@@ -7,5 +7,15 @@ namespace Destiny2.Responses
     {
         [JsonProperty(PropertyName = "profile")]
         public SingleComponentResponse<DestinyProfileComponent> Profile { get; set; } = null;
+        [JsonProperty(PropertyName = "profileInventory")]
+        public SingleComponentResponse<DestinyInventoryComponent> ProfileInventory { get; set; } = null;
+        [JsonProperty(PropertyName = "characters")]
+        public DictionaryComponentResponseOfInt64<DestinyCharacterComponent> Characters { get; set; } = null;
+        [JsonProperty(PropertyName = "characterInventories")]
+        public DictionaryComponentResponseOfInt64<DestinyInventoryComponent> CharacterInventories { get; set; } = null;
+        [JsonProperty(PropertyName = "characterEquipment")]
+        public DictionaryComponentResponseOfInt64<DestinyInventoryComponent> CharacterEquipment { get; set; } = null;
+        [JsonProperty(PropertyName = "itemComponents")]
+        public DestinyItemComponentSetOfInt64 ItemComponents { get; set; } = null;
     }
 }
