3e87f562487f07c568c02c3c83bdf4affac5b1d7
Andy Schott <andy@andyschott.com>
2019-06-15 18:02:06 -0400

    externalize the base url

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -5,8 +5,12 @@ namespace Destiny2.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddDestiny(this IServiceCollection services, string apiKey)
+        public static void AddDestiny(this IServiceCollection services, string baseUrl, string apiKey)
         {
+            if(string.IsNullOrEmpty(baseUrl))
+            {
+                throw new ArgumentException("Must specify the base URL", "baseUrl");
+            }
             if(string.IsNullOrEmpty(apiKey))
             {
                 throw new ArgumentException("Must specify an API Key", "apiKey");
@@ -14,7 +18,7 @@ namespace Destiny2.Extensions
 
             services.AddHttpClient<IDestiny, Destiny>(client =>
             {
-                client.BaseAddress = new Uri("https://www.bungie.net");
+                client.BaseAddress = new Uri(baseUrl);
                 client.DefaultRequestHeaders.Add("X-API-Key", apiKey);
             });
         }
