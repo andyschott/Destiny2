22e5b3a49bc07ccbfce093fb61c777bdade157c9
Andy Schott <andy@andyschott.com>
2019-09-08 19:14:29 -0400

    add ProfileCurrencies to DestinyProfileResponse

--- Responses/DestinyProfileResponse.cs
+++ Responses/DestinyProfileResponse.cs
@@ -1,21 +1,15 @@
 using Destiny2.Entities;
-using Newtonsoft.Json;
 
 namespace Destiny2.Responses
 {
     public class DestinyProfileResponse
     {
-        [JsonProperty(PropertyName = "profile")]
         public SingleComponentResponse<DestinyProfileComponent> Profile { get; set; } = null;
-        [JsonProperty(PropertyName = "profileInventory")]
         public SingleComponentResponse<DestinyInventoryComponent> ProfileInventory { get; set; } = null;
-        [JsonProperty(PropertyName = "characters")]
         public DictionaryComponentResponseOfInt64<DestinyCharacterComponent> Characters { get; set; } = null;
-        [JsonProperty(PropertyName = "characterInventories")]
         public DictionaryComponentResponseOfInt64<DestinyInventoryComponent> CharacterInventories { get; set; } = null;
-        [JsonProperty(PropertyName = "characterEquipment")]
         public DictionaryComponentResponseOfInt64<DestinyInventoryComponent> CharacterEquipment { get; set; } = null;
-        [JsonProperty(PropertyName = "itemComponents")]
         public DestinyItemComponentSetOfInt64 ItemComponents { get; set; } = null;
+        public SingleComponentResponse<DestinyInventoryComponent> ProfileCurrencies { get; set; }
     }
 }
