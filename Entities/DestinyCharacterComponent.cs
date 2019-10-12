69dcc5779487ad8d5b319da2d11bb6a09a475503
Andy Schott <andy@andyschott.com>
2019-10-12 16:07:38 -0400

    add character stats

--- Entities/DestinyCharacterComponent.cs
+++ Entities/DestinyCharacterComponent.cs
@@ -1,29 +1,16 @@
-using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Destiny2.Entities
 {
     public class DestinyCharacterComponent
     {
-        [JsonProperty(PropertyName = "light")]
         public int Light { get; set; }
-
-        [JsonProperty(PropertyName = "raceHash")]
         public uint RaceHash { get; set; }
-
-        [JsonProperty(PropertyName = "genderHash")]
         public uint GenderHash { get; set; }
-
-        [JsonProperty(PropertyName = "classHash")]
         public uint ClassHash { get; set; }
-
-        [JsonProperty(PropertyName = "emblemPath")]
         public string EmblemPath { get; set; }
-
-        [JsonProperty(PropertyName = "emblemBackgroundPath")]
         public string EmblemBackgroundPath { get; set; }
-
-        [JsonProperty(PropertyName = "baseCharacterLevel")]
         public int Level { get; set; }
-        
+        public IDictionary<uint, int> Stats { get; set; }
     }
 }
