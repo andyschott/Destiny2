b8272159f4555a8e53a344eeee6048e63e5a5c15
Michael Aird <michael.aird@gmail.com>
2021-03-07 11:14:41 -0500

    Including Activities in CharacterResponse

---
+++ Entities/Characters/DestinyCharacterActivitiesComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Destiny2.Entities.Characters
+{
+    public class DestinyCharacterActivitiesComponent
+    {
+        public IEnumerable<DestinyActivity> AvailableActivities { get; set; }
+
+            //dateActivityStarted
+            //currentActivityHash
+            //currentActivityModeHash
+            //currentActivityModeType
+            //currentActivityModeHashes
+            //currentActivityModeTypes
+            //currentPlaylistActivityHash
+            //lastCompletedStoryHash
+    }
+}
